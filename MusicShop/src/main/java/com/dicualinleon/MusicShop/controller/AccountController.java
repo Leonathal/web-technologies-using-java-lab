0a949a03331f61fcc6c7294cd5039d7a01bee76f
Yamihiro <dicualinleon@gmail.com>
2021-01-12 19:54:23 +0200

    Added validation + validation for account

--- MusicShop/src/main/java/com/dicualinleon/MusicShop/controller/AccountController.java
+++ MusicShop/src/main/java/com/dicualinleon/MusicShop/controller/AccountController.java
@@ -8,6 +8,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/account")
 public class AccountController {
@@ -19,14 +21,20 @@ public class AccountController {
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<AccountDto> create(@RequestBody AccountDto accountDto) {
+    public ResponseEntity<AccountDto> create(@RequestBody @Valid AccountDto accountDto) {
         AccountDto savedAccountDto = accountService.save(accountDto);
-        return new ResponseEntity<>(savedAccountDto, null == savedAccountDto ? HttpStatus.EXPECTATION_FAILED : HttpStatus.CREATED);
+        //return new ResponseEntity<>(savedAccountDto, null == savedAccountDto ? HttpStatus.EXPECTATION_FAILED : HttpStatus.CREATED);
+        return ResponseEntity
+                .ok()
+                .body(savedAccountDto);
     }
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<AccountDto> get(@PathVariable("id") Long id) {
         AccountDto accountDto = accountService.get(id);
-        return new ResponseEntity<>(accountDto, null == accountDto ? HttpStatus.NOT_FOUND : HttpStatus.OK);
+        //return new ResponseEntity<>(accountDto, null == accountDto ? HttpStatus.NOT_FOUND : HttpStatus.OK);
+        return ResponseEntity
+                .ok()
+                .body(accountDto);
     }
 }
--- MusicShop/src/main/java/com/dicualinleon/MusicShop/dto/AccountDto.java
+++ MusicShop/src/main/java/com/dicualinleon/MusicShop/dto/AccountDto.java
@@ -3,11 +3,21 @@ package com.dicualinleon.MusicShop.dto;
 import lombok.Builder;
 import lombok.Getter;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Getter
 @Builder
 public class AccountDto {
 
+    @NotNull(message = "The username field is required.")
+    @Size(min = 2, max = 30)
     final private String username;
+
+    @NotNull(message = "The password field is required.")
+    @Size(min = 6, max = 30)
     final private String password;
+
+    @NotNull(message = "The email field is required.")
     final private String email;
 }
