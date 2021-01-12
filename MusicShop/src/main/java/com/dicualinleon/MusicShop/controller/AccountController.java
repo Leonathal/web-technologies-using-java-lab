0d8b1459722b45d59d4409acba619a6534e4c19e
Yamihiro <dicualinleon@gmail.com>
2021-01-12 18:48:22 +0200

    AccountController update
    Added PostMethod for creating an account

--- MusicShop/src/main/java/com/dicualinleon/MusicShop/controller/AccountController.java
+++ MusicShop/src/main/java/com/dicualinleon/MusicShop/controller/AccountController.java
@@ -4,11 +4,9 @@ import com.dicualinleon.MusicShop.dto.AccountDto;
 import com.dicualinleon.MusicShop.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/account")
@@ -18,6 +16,12 @@ public class AccountController {
 
     public AccountController(AccountService accountService) {
         this.accountService = accountService;
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<AccountDto> create(@RequestBody AccountDto accountDto) {
+        AccountDto savedAccountDto = accountService.save(accountDto);
+        return new ResponseEntity<>(savedAccountDto, null == savedAccountDto ? HttpStatus.EXPECTATION_FAILED : HttpStatus.CREATED);
     }
 
     @GetMapping(value = "/{id}")
